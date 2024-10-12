1fefbed20cc24e89300cfad295638d6f01baf3b0
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-12 17:00:14 +0200

    Add retrieve after option

--- src/Spottarr.Services/Configuration/SpotnetOptions.cs
+++ src/Spottarr.Services/Configuration/SpotnetOptions.cs
@@ -8,4 +8,5 @@ public class SpotnetOptions
     public string CommentGroup { get; init; } = "";
     public string ReportGroup { get; init; } = "";
     public string NzbGroup { get; init; } = "";
+    public DateOnly RetrieveAfter { get; init; } = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
 }
