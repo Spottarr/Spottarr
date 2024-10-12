67da74e530983383f1b4f555d8b04997b63cacd6
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-12 15:38:28 +0200

    Add NNTP client and connect

--- src/Spottarr.Services/SpotnetService.cs
+++ src/Spottarr.Services/SpotnetService.cs
@@ -1,17 +1,36 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Spottarr.Services.Configuration;
 using Spottarr.Services.Contracts;
+using Usenet.Nntp;
 
 namespace Spottarr.Services;
 
 internal sealed class SpotnetService : ISpotnetService
 {
     private readonly IOptions<UsenetOptions> _usenetOptions;
-    
-    public SpotnetService(IOptions<UsenetOptions> usenetOptions) => _usenetOptions = usenetOptions;
+    private readonly IOptions<SpotnetOptions> _spotnetOptions;
 
-    public Task Import()
+    public SpotnetService(ILoggerFactory loggerFactory, IOptions<UsenetOptions> usenetOptions, IOptions<SpotnetOptions> spotnetOptions)
     {
-        throw new NotImplementedException();
+        _usenetOptions = usenetOptions;
+        _spotnetOptions = spotnetOptions;
+        
+        // Enable NNTP client logging
+        Usenet.Logger.Factory = loggerFactory;
+    }
+
+    public async Task Import()
+    {
+        // Set up Usenet connection
+        using var connection = new NntpConnection();
+        var client = new NntpClient(connection);
+
+        var usenetOptions = _usenetOptions.Value;
+        await client.ConnectAsync(usenetOptions.Hostname, usenetOptions.Port, usenetOptions.UseTls);
+        client.Authenticate(usenetOptions.Username, usenetOptions.Password);
+
+        var spotnetOptions = _spotnetOptions.Value;
+        client.Group(spotnetOptions.SpotGroup);
     }
 }
