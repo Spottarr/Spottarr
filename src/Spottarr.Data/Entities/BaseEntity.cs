9f46ae0af9e53a76bea70e5adccd342bb2df4d8b
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-13 15:57:15 +0200

    Remove database generated values option for dates

--- src/Spottarr.Data/Entities/BaseEntity.cs
+++ src/Spottarr.Data/Entities/BaseEntity.cs
@@ -1,13 +1,9 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Spottarr.Data.Entities;
 
 public abstract class BaseEntity
 {
     public int Id { get; set; }
     
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTimeOffset CreatedAt { get; set; }
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTimeOffset UpdatedAt { get; set; }
 }
