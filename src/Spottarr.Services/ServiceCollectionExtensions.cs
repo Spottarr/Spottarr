71bce899b76c75f598b542b98542be16698e1bdd
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-13 15:38:08 +0200

    Fix dependency lifetime

--- src/Spottarr.Services/ServiceCollectionExtensions.cs
+++ src/Spottarr.Services/ServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@ public static class ServiceCollectionExtensions
         ArgumentNullException.ThrowIfNull(configuration);
         
         return services
-            .AddSingleton<ISpotnetService, SpotnetService>()
+            .AddScoped<ISpotnetService, SpotnetService>()
             .Configure<UsenetOptions>(configuration.GetSection(UsenetOptions.Section))
             .Configure<SpotnetOptions>(configuration.GetSection(SpotnetOptions.Section));
     }
