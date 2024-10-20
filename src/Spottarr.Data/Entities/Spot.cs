1c40f79fe262754552e3a8c36cac0f4579e0b675
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-20 12:33:35 +0200

    Add years, seasons and episodes to spot

--- src/Spottarr.Data/Entities/Spot.cs
+++ src/Spottarr.Data/Entities/Spot.cs
@@ -7,16 +7,14 @@ public abstract class Spot : BaseEntity
     public required string Title { get; set; }
     public string? Description { get; set; }
     public required string Spotter { get; set; }
-    
     public required long Bytes { get; set; }
     public required string MessageId { get; set; }
     public required long MessageNumber { get; set; }
-    
     public SpotType Type { get; set; }
-    
     public FtsSpot? FtsSpot { get; set; }
-    
+    public ICollection<int> Years { get; init; } = [];
+    public ICollection<int> Seasons { get; init; } = [];
+    public ICollection<int> Episodes { get; init; } = [];
     public required DateTime SpottedAt { get; set; }
-    
     public DateTime? IndexedAt { get; set; }
 }
---
+++ src/Spottarr.Data/Migrations/20241020103247_AddYearsSeasonsEpisodes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spottarr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYearsSeasonsEpisodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Episodes",
+                table: "Spots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Seasons",
+                table: "Spots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Years",
+                table: "Spots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Episodes",
+                table: "Spots");
+
+            migrationBuilder.DropColumn(
+                name: "Seasons",
+                table: "Spots");
+
+            migrationBuilder.DropColumn(
+                name: "Years",
+                table: "Spots");
+        }
+    }
+}
