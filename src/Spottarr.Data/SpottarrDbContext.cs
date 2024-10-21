305cb9d3f3fc45efa500d77b8a2e5ad72580b976
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-21 19:42:05 +0200

    Remove unused db set

--- src/Spottarr.Data/SpottarrDbContext.cs
+++ src/Spottarr.Data/SpottarrDbContext.cs
@@ -12,7 +12,6 @@ public class SpottarrDbContext : DbContext
     private readonly string _dbPath;
 
     public DbSet<Spot> Spots { get; set; }
-    public DbSet<Spot> NzbFiles { get; set; }
     public DbSet<FtsSpot> FtsSpots { get; set; }
 
     public SpottarrDbContext(IHostEnvironment environment, ILoggerFactory loggerFactory)
