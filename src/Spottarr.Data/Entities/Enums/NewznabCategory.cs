ea3fe1f6f5efe48b6a75be1a66f3cd6efd84cd2b
Christiaan de Ridder <christiaan@channelengine.com>
2024-10-22 16:51:24 +0200

    Clean up newznab category names

--- src/Spottarr.Data/Entities/Enums/NewznabCategory.cs
+++ src/Spottarr.Data/Entities/Enums/NewznabCategory.cs
@@ -10,175 +10,175 @@ public enum NewznabCategory
     [Display(Name = "Console")]
     Console = 1000,
 
-    [Display(Name = "Console/NDS")]
+    [Display(Name = "NDS")]
     ConsoleNds = 1010,
 
-    [Display(Name = "Console/PSP")]
+    [Display(Name = "PSP")]
     ConsolePsp = 1020,
 
-    [Display(Name = "Console/Wii")]
+    [Display(Name = "Wii")]
     ConsoleWii = 1030,
 
-    [Display(Name = "Console/Switch")]
+    [Display(Name = "Switch")]
     ConsoleSwitch = 1035,
 
-    [Display(Name = "Console/XBox")]
+    [Display(Name = "XBox")]
     ConsoleXBox = 1040,
 
-    [Display(Name = "Console/XBox 360")]
+    [Display(Name = "XBox 360")]
     ConsoleXBox360 = 1050,
 
-    [Display(Name = "Console/Wiiware")]
+    [Display(Name = "Wiiware")]
     ConsoleWiiware = 1060,
 
-    [Display(Name = "Console/XBox 360 DLC")]
+    [Display(Name = "XBox 360 DLC")]
     ConsoleXBox360Dlc = 1070,
 
-    [Display(Name = "Console/PS3")]
+    [Display(Name = "PS3")]
     ConsolePs3 = 1080,
 
-    [Display(Name = "Console/XBox One")]
+    [Display(Name = "XBox One")]
     ConsoleXBoxOne = 1090,
 
-    [Display(Name = "Console/PS4")]
+    [Display(Name = "PS4")]
     ConsolePs4 = 1100,
 
     [Display(Name = "Movies")]
     Movies = 2000,
 
-    [Display(Name = "Movies/Foreign")]
+    [Display(Name = "Foreign")]
     MoviesForeign = 2010,
 
-    [Display(Name = "Movies/Other")]
+    [Display(Name = "Other")]
     MoviesOther = 2020,
 
-    [Display(Name = "Movies/SD")]
+    [Display(Name = "SD")]
     MoviesSd = 2030,
 
-    [Display(Name = "Movies/HD")]
+    [Display(Name = "HD")]
     MoviesHd = 2040,
 
-    [Display(Name = "Movies/UHD")]
+    [Display(Name = "UHD")]
     MoviesUhd = 2045,
 
-    [Display(Name = "Movies/BluRay")]
+    [Display(Name = "BluRay")]
     MoviesBluRay = 2050,
 
-    [Display(Name = "Movies/3D")]
+    [Display(Name = "3D")]
     Movies3D = 2060,
 
     [Display(Name = "Audio")]
     Audio = 3000,
 
-    [Display(Name = "Audio/MP3")]
+    [Display(Name = "MP3")]
     AudioMp3 = 3010,
 
-    [Display(Name = "Audio/Video")]
+    [Display(Name = "Video")]
     AudioVideo = 3020,
 
-    [Display(Name = "Audio/Audiobook")]
+    [Display(Name = "Audiobook")]
     AudioAudiobook = 3030,
 
-    [Display(Name = "Audio/Lossless")]
+    [Display(Name = "Lossless")]
     AudioLossless = 3040,
 
-    [Display(Name = "Audio/Podcast")]
+    [Display(Name = "Podcast")]
     AudioPodcast = 3050,
 
     [Display(Name = "PC")]
     Pc = 4000,
 
-    [Display(Name = "PC/0day")]
+    [Display(Name = "0day")]
     Pc0day = 4010,
 
-    [Display(Name = "PC/ISO")]
+    [Display(Name = "ISO")]
     PcIso = 4020,
 
-    [Display(Name = "PC/Mac")]
+    [Display(Name = "Mac")]
     PcMac = 4030,
 
-    [Display(Name = "PC/Mobile-Other")]
+    [Display(Name = "Mobile - Other")]
     PcMobileOther = 4040,
 
-    [Display(Name = "PC/Games")]
+    [Display(Name = "Games")]
     PcGames = 4050,
 
-    [Display(Name = "PC/Mobile-iOS")]
+    [Display(Name = "Mobile - iOS")]
     PcMobileiOs = 4060,
 
-    [Display(Name = "PC/Mobile-Android")]
+    [Display(Name = "Mobile - Android")]
     PcMobileAndroid = 4070,
 
     [Display(Name = "TV")]
     Tv = 5000,
 
-    [Display(Name = "TV/Foreign")]
+    [Display(Name = "Foreign")]
     TvForeign = 5020,
 
-    [Display(Name = "TV/SD")]
+    [Display(Name = "SD")]
     TvSd = 5030,
 
-    [Display(Name = "TV/HD")]
+    [Display(Name = "HD")]
     TvHd = 5040,
 
-    [Display(Name = "TV/UHD")]
+    [Display(Name = "UHD")]
     TvUhd = 5045,
 
-    [Display(Name = "TV/Other")]
+    [Display(Name = "Other")]
     TvOther = 5050,
 
-    [Display(Name = "TV/Sport")]
+    [Display(Name = "Sport")]
     TvSport = 5060,
 
-    [Display(Name = "TV/Anime")]
+    [Display(Name = "Anime")]
     TvAnime = 5070,
 
-    [Display(Name = "TV/Documentary")]
+    [Display(Name = "Documentary")]
     TvDocumentary = 5080,
 
     [Display(Name = "XXX")]
     Xxx = 6000,
 
-    [Display(Name = "XXX/DVD")]
+    [Display(Name = "DVD")]
     XxxDvd = 6010,
 
-    [Display(Name = "XXX/WMV")]
+    [Display(Name = "WMV")]
     XxxWmv = 6020,
 
-    [Display(Name = "XXX/XviD")]
+    [Display(Name = "XviD")]
     XxxXviD = 6030,
 
-    [Display(Name = "XXX/x264")]
+    [Display(Name = "x264")]
     XxxX264 = 6040,
 
-    [Display(Name = "XXX/Pack")]
+    [Display(Name = "Pack")]
     XxxPack = 6050,
 
-    [Display(Name = "XXX/ImgSet")]
+    [Display(Name = "ImgSet")]
     XxxImgSet = 6060,
 
-    [Display(Name = "XXX/Other")]
+    [Display(Name = "Other")]
     XxxOther = 6070,
 
     [Display(Name = "Books")]
     Books = 7000,
 
-    [Display(Name = "Books/Mags")]
+    [Display(Name = "Mags")]
     BooksMags = 7010,
 
-    [Display(Name = "Books/EBook")]
+    [Display(Name = "EBook")]
     BooksEBook = 7020,
 
-    [Display(Name = "Books/Comics")]
+    [Display(Name = "Comics")]
     BooksComics = 7030,
 
     [Display(Name = "Other")]
     Other = 8000,
 
-    [Display(Name = "Other/Misc")]
+    [Display(Name = "Misc")]
     OtherMisc = 8010,
 
-    [Display(Name = "Category Not Determined")]
+    [Display(Name = "Category not determined")]
     CategoryNotDetermined = 7900,
 
     [Display(Name = "Custom")]
