a7ed3e38b152af3baf8e4170430cf6d11da47ef5
Christiaan de Ridder <christiaan@channelengine.com>
2024-11-02 15:22:12 +0100

    Clean up initialization logic

--- src/Spottarr.Web/Program.cs
+++ src/Spottarr.Web/Program.cs
@@ -1,52 +1,15 @@
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.FileProviders;
 using Spottarr.Data.Helpers;
 using Spottarr.Services;
-using Spottarr.Services.Helpers;
+using Spottarr.Web.Helpers;
+using Spottarr.Web.Logging;
 using Spottarr.Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsContainer())
-{
-    builder.Logging.AddSimpleConsole(options =>
-    {
-        options.SingleLine = true;
-        options.TimestampFormat = "HH:mm:ss ";
-    });
-}
-else
-{
-    builder.Logging.AddConsole();
-}
-
-if (builder.Environment.IsDevelopment() || builder.Environment.IsContainerFastMode())
-{
-    // ASP.NET expects the configuration files to be in the root of the project when running an app from source.
-    // Because we share the config file for the entire solution we need to read it from the bin directory like
-    // a console app does instead.
-    var root = AppContext.BaseDirectory;
-
-    foreach (var json in builder.Configuration.Sources.OfType<JsonConfigurationSource>())
-        json.FileProvider = new PhysicalFileProvider(root);
-
-    if (builder.Configuration is IConfigurationRoot configRoot)
-        configRoot.Reload();
-}
-
-var mvcBuilder = builder.Services.AddControllersWithViews()
-    .AddXmlSerializerFormatters();
-
-if (builder.Environment.IsDevelopment())
-    mvcBuilder.AddRazorRuntimeCompilation();
-
-builder.Services.Configure<RouteOptions>(options =>
-{
-    options.LowercaseQueryStrings = true;
-    options.LowercaseUrls = true;
-});
-
+builder.Logging.AddConsole(builder.Environment);
+builder.Configuration.MapConfigurationSources(builder.Environment);
 builder.Services.AddSpottarrServices(builder.Configuration);
+builder.Services.AddSpottarrWeb(builder.Environment);
 
 var app = builder.Build();
 
