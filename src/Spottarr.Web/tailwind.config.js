c7f55aa17be878acb54341a0b2725ec7766c28c7
Christiaan de Ridder <christiaan@channelengine.com>
2024-11-03 17:13:09 +0100

    Add basic layout

--- src/Spottarr.Web/tailwind.config.js
+++ src/Spottarr.Web/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './**/*.{razor,html}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
 }
