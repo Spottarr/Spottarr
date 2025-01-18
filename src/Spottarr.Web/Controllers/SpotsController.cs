1604fdce7cd823aefbb18b2def6517b1a67acf65
Christiaan de Ridder <christiaan@channelengine.com>
2025-01-18 18:28:29 +0100

    Add basic API controller

---
+++ src/Spottarr.Web/Controllers/SpotsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Spottarr.Data.Entities.Enums;
+using Spottarr.Services.Contracts;
+using Spottarr.Services.Models;
+
+namespace Spottarr.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class SpotsController : Controller
+{
+    private readonly ISpotSearchService _spotSearchService;
+
+    public SpotsController(ISpotSearchService spotSearchService) => _spotSearchService = spotSearchService;
+
+    [HttpGet]
+    public async Task<IActionResult> Index(
+        string? query,
+        int page = 0,
+        int? episode = null,
+        int? season = null,
+        int? year = null,
+        ISet<NewznabCategory>? newznabCategories = null
+    )
+    {
+        var results = await _spotSearchService.Search(new SpotSearchFilter
+        {
+            Offset = page,
+            Limit = 100,
+            Query = query,
+            Categories = newznabCategories?.ToHashSet(),
+            Years = year.HasValue ? [year.Value] : null,
+            Episodes = episode.HasValue ? [episode.Value] : null,
+            Seasons = season.HasValue ? [season.Value] : null,
+        });
+
+        return Json(results);
+    }
+}
