e479dc109e8975c61cc4ac781cf8c926c6787ffb
Christiaan de Ridder <christiaan@channelengine.com>
2025-02-03 20:09:32 +0100

    Add flowbite

--- src/Spottarr.Web.Client/tailwind.config.js
+++ src/Spottarr.Web.Client/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+import flowbite from 'flowbite/plugin';
+
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+  ],
   safelist: [
     {
       pattern: /bg-(sky|emerald|amber|red|fuchsia)/,
-      variants: ['dark']
+      variants: ['dark'],
     },
   ],
   theme: {
@@ -16,5 +23,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite],
 };
