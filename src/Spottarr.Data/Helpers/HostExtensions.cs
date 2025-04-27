18f56e202042e9d720f332df70f5f3048d6a588c
Christiaan de Ridder <christiaan@channelengine.com>
2025-04-27 12:45:27 +0200

    Move DB migration helper to Spottarr.Data

---
+++ src/Spottarr.Data/Helpers/HostExtensions.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Spottarr.Data.Logging;
+
+namespace Spottarr.Data.Helpers;
+
+public static class HostExtensions
+{
+    public static async Task MigrateDatabase(this IHost host)
+    {
+        ArgumentNullException.ThrowIfNull(host);
+
+        await using var scope = host.Services.CreateAsyncScope();
+
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<IHost>>();
+        logger.DatabaseMigrationStarted(DbPathHelper.GetDbPath());
+        var dbContext = scope.ServiceProvider.GetRequiredService<SpottarrDbContext>();
+        await dbContext.Database.MigrateAsync();
+        logger.DatabaseMigrationFinished();
+    }
+}
---
+++ src/Spottarr.Data/Logging/LoggerExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Logging;
+
+namespace Spottarr.Data.Logging;
+
+internal static partial class LoggerExtensions
+{
+    [LoggerMessage(Level = LogLevel.Information, Message = "Database migration started [{DbPath}].")]
+    public static partial void DatabaseMigrationStarted(this ILogger logger, string dbPath);
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Database migration finished.")]
+    public static partial void DatabaseMigrationFinished(this ILogger logger);
+}
