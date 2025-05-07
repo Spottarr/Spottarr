14e201b62eac1bc524bf344606548466d3125e8d
Christiaan de Ridder <christiaan@channelengine.com>
2025-05-07 20:44:16 +0200

    Allow multiple query parameter constraint attributes

--- src/Spottarr.Web/Helpers/QueryParameterConstraintAttribute.cs
+++ src/Spottarr.Web/Helpers/QueryParameterConstraintAttribute.cs
@@ -2,24 +2,24 @@ using Microsoft.AspNetCore.Mvc.ActionConstraints;
 
 namespace Spottarr.Web.Helpers;
 
-[AttributeUsage(AttributeTargets.Method)]
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 internal sealed class QueryParameterConstraintAttribute : Attribute, IActionConstraint
 {
     // Ensure this constraint runs early
     public int Order => 0;
     public string Parameter { get; }
-    public string[] Values { get; }
+    public string Value { get; }
 
-    public QueryParameterConstraintAttribute(string parameter, params string[] values)
+    public QueryParameterConstraintAttribute(string parameter, string value)
     {
         Parameter = parameter;
-        Values = values;
+        Value = value;
     }
 
     public bool Accept(ActionConstraintContext context)
     {
         var query = context.RouteContext.HttpContext.Request.Query;
         return query.TryGetValue(Parameter, out var actualValues)
-               && actualValues.Intersect(Values).Any();
+               && Value.Equals(actualValues, StringComparison.OrdinalIgnoreCase);
     }
 }
