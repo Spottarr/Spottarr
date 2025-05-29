7635a63adc458151d266fe1e54f05056755ad604
Christiaan de Ridder <christiaan@channelengine.com>
2025-05-29 14:33:20 +0200

    Put SQLite DB in a directory when running outside of docker

--- src/Spottarr.Data/Helpers/DbPathHelper.cs
+++ src/Spottarr.Data/Helpers/DbPathHelper.cs
@@ -5,7 +5,10 @@ public static class DbPathHelper
     public static string GetDbPath()
     {
         var isContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
-        var root = isContainer ? "/data" : Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var root = isContainer
+            ? "/data"
+            : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "spottarr");
+        if (!Directory.Exists(root)) Directory.CreateDirectory(root);
         return Path.Join(root, "spottarr.db");
     }
 }
