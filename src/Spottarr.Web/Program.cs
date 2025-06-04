bc1ca55a2f1377f583e6b3d3f311865ce1122f28
Christiaan de Ridder <christiaan@channelengine.com>
2025-06-04 20:01:08 +0200

    Fix typo

--- src/Spottarr.Web/Program.cs
+++ src/Spottarr.Web/Program.cs
@@ -29,7 +29,7 @@ app.MapScalarApiReference();
 // Middleware pipeline, order matters here
 app.UseForwardedHeaders();
 app.UseDefaultFiles();
-app.UseMiddleware<NewsznabQueryActionMiddleware>();
+app.UseMiddleware<NewznabQueryActionMiddleware>();
 app.UseRouting();
 app.UseCors();
 app.UseAntiforgery();
