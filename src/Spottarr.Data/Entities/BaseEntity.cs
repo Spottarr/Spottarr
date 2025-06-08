aaa0a4f2f7ff2522b54005e077542244b7ff2705
Christiaan de Ridder <christiaan@channelengine.com>
2025-06-08 12:21:42 +0200

    Remove required modifier to be able to map the Spot entity in bulk upserts

--- src/Spottarr.Data/Entities/BaseEntity.cs
+++ src/Spottarr.Data/Entities/BaseEntity.cs
@@ -3,7 +3,7 @@ namespace Spottarr.Data.Entities;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    
-    public required DateTime CreatedAt { get; init; }
-    public required DateTime UpdatedAt { get; set; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; set; }
 }
