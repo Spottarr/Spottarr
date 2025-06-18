4fd10c2e655b0e84707fae2a78e9d1a4118d1bc5
Christiaan de Ridder <christiaan@channelengine.com>
2025-06-18 20:16:21 +0200

    Add configuration binder source generator to support AOT down the road

--- src/Spottarr.Services/Configuration/SpotnetOptions.cs
+++ src/Spottarr.Services/Configuration/SpotnetOptions.cs
@@ -4,35 +4,35 @@ public class SpotnetOptions
 {
     public const string Section = "Spotnet";
 
-    public required string SpotGroup { get; init; }
-    public required string CommentGroup { get; init; }
-    public required string ReportGroup { get; init; }
-    public required string NzbGroup { get; init; }
-    public required bool ImportAdultContent { get; init; }
+    public string SpotGroup { get; init; } = string.Empty;
+    public string CommentGroup { get; init; } = string.Empty;
+    public string ReportGroup { get; init; } = string.Empty;
+    public string NzbGroup { get; init; } = string.Empty;
+    public bool ImportAdultContent { get; init; }
 
     /// <summary>
     /// The maximum age of spots to retrieve
     /// Defaults to 30 days
     /// </summary>
-    public required DateTimeOffset RetrieveAfter { get; init; } = DateTimeOffset.Now.AddDays(-30);
+    public DateTimeOffset RetrieveAfter { get; init; } = DateTimeOffset.Now.AddDays(-30);
 
     /// <summary>
     /// The maximum number of spots to retrieve per batch during import
     /// </summary>
-    public required int ImportBatchSize { get; init; }
+    public int ImportBatchSize { get; init; }
 
     /// <summary>
     /// The number of days to keep spots in the database
     /// </summary>
-    public required int RetentionDays { get; init; }
+    public int RetentionDays { get; init; }
 
     /// <summary>
     /// The cron schedule for the spot import and index job
     /// </summary>
-    public required string ImportSpotsSchedule { get; init; }
+    public string ImportSpotsSchedule { get; init; } = string.Empty;
 
     /// <summary>
     /// The cron schedule for the spot clean up and database maintenance job
     /// </summary>
-    public required string CleanUpSpotsSchedule { get; init; }
+    public string CleanUpSpotsSchedule { get; init; } = string.Empty;
 }
